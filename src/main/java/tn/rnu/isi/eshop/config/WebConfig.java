dfc8d36fc0e0d070d019d149d901d9b1dbb53422
Adel1818 <adelelechi9@gmail.com>
2021-01-19 00:12:29 +0100

    fix pb cross origin

--- src/main/java/tn/rnu/isi/eshop/config/WebConfig.java
+++ src/main/java/tn/rnu/isi/eshop/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://adel-elechi-front-e-shop.herokuapp.com");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
